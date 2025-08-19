e9292d3533eb2ae57a297da2b18390823c1b129c
Karavaev <karavaev@inpas.ru>
2025-08-20 00:32:03 +0300

    Test

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,7 +1,7 @@
 package ru.otus.homework
 
 fun main() {
-    println(sumOrThrow(-1, -2)) // IllegalArgumentException: Only positive numbers are allowed
+    println(sumOrThrow(-1, -1)) // IllegalArgumentException: Only positive numbers are allowed
     println(sumOrThrow(Int.MAX_VALUE, Int.MAX_VALUE)) // IllegalStateException: Sum overflow
 }
 
